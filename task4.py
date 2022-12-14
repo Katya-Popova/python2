84e8797ce3efde72742b7140ae2a03ac31feff7f
Katya Popova <Katya_ Popova@mail.ru>
2022-12-14 14:57:29 +0300

    Добавлено  решение задачи

--- task4.py
+++ task4.py
@@ -1,19 +1,3 @@
-
-
-
-
-# Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
-n = int(input())
-y = 0
-x = 0
-some_list = []
-for i in range(1, n+1):
-    y = ((1+1/i)**i)
-    some_list.append(y)
-    x = x+y
-print(some_list)
-print('Сумма:', x)
-
 # Реализуйте алгоритм перемешивания списка
 import random
 some_list = [1, 5, 'erag', 54, 6]
